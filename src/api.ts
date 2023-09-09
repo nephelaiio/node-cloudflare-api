63830c872653f0f6e03137184b75ecb47ac85f81
Ted Cook <teodoro.cook@gmail.com>
2023-09-09 11:40:46 -0600

    Add wait delay to pai retry function

--- src/api.ts
+++ src/api.ts
@@ -3,11 +3,18 @@
 import { debug, info, error } from '@nephelaiio/logger';
 
 const TIMEOUT_DEFAULT = 5000;
+const DELAY_DEFAULT = 1000;
 const RETRIES_DEFAULT = 3;
 
 type Method = 'GET' | 'POST' | 'PUT' | 'PATCH' | 'DELETE' | 'HEAD';
 
-async function retry(fn: () => Promise<any>, times: number = RETRIES_DEFAULT) {
+function wait(period: number){
+    return new Promise(resolve => {
+        setTimeout(resolve, period);
+    });
+}
+
+async function retry(fn: () => Promise<any>, times: number, delay: number) {
   try {
     return await fn();
   } catch (e: any) {
@@ -15,7 +22,8 @@ async function retry(fn: () => Promise<any>, times: number = RETRIES_DEFAULT) {
       debug(e.message);
       throw e;
     } else {
-      return await retry(fn, times - 1);
+      await wait(delay);
+      await retry(fn, times - 1);
     }
   }
 }
@@ -36,6 +44,8 @@ type ApiOptions = {
   method?: Method;
   body?: object | null;
   ignore_errors?: Array<number>;
+  retries?: number;
+  delay?: number;
 };
 
 async function api(options: ApiOptions): Promise<any> {
@@ -44,7 +54,9 @@ async function api(options: ApiOptions): Promise<any> {
     path,
     method = 'GET',
     body = null,
-    ignore_errors = []
+    ignore_errors = [],
+    retries = RETRIES_DEFAULT,
+    delay = DELAY_DEFAULT
   } = options;
   const uri = `https://api.cloudflare.com/client/v4${path}`;
   info(`Fetching ${method} ${uri}`);
@@ -55,7 +67,7 @@ async function api(options: ApiOptions): Promise<any> {
     };
     const options = { method, body, headers };
     try {
-      const response = await retry(async () => await timedFetch(url, options));
+      const response = await retry(async () => await timedFetch(url, options), retries, delay);
       if (response.ok || ignore_errors.some((x) => x == response.status)) {
         info(`Got response ${response.status} for ${method} ${uri}`);
         return response;
