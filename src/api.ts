52b9bf55bc57ba8a256ed0736971342179546620
Ted Cook <teodoro.cook@gmail.com>
2023-09-11 21:34:26 -0600

    Add max page size constant export

--- src/api.ts
+++ src/api.ts
@@ -7,37 +7,7 @@ const DELAY_DEFAULT = 1000;
 const RETRIES_DEFAULT = 3;
 
 type Method = 'GET' | 'POST' | 'PUT' | 'PATCH' | 'DELETE' | 'HEAD';
-
-function wait(period: number) {
-  return new Promise((resolve) => {
-    setTimeout(resolve, period);
-  });
-}
-
-async function retry(fn: () => Promise<any>, times: number, delay: number) {
-  try {
-    return await fn();
-  } catch (e: any) {
-    if (times <= 0) {
-      debug(e.message);
-      throw e;
-    } else {
-      await wait(delay);
-      await retry(fn, times - 1, delay);
-    }
-  }
-}
-
-async function timedFetch(resource: string, options: any) {
-  const { timeout = TIMEOUT_DEFAULT } = options;
-  const controller = new AbortController();
-  const signal = controller.signal;
-  const id = setTimeout(() => controller.abort(), timeout);
-  const response = await fetch(resource, { ...options, signal });
-  clearTimeout(id);
-  return response;
-}
-
+type AsyncFn = () => Promise<any>;
 type ApiOptions = {
   token: string;
   path: string;
@@ -48,72 +18,100 @@ type ApiOptions = {
   delay?: number;
 };
 
-async function api(options: ApiOptions): Promise<any> {
-  const {
-    token,
-    path,
-    method = 'GET',
-    body = null,
-    ignore_errors = [],
-    retries = RETRIES_DEFAULT,
-    delay = DELAY_DEFAULT
-  } = options;
-  const uri = `https://api.cloudflare.com/client/v4${path}`;
-  info(`Fetching ${method} ${uri}`);
-  async function fetchData(url: string) {
-    const headers = {
-      'Content-Type': 'application/json',
-      Authorization: `Bearer ${token}`
-    };
-    const options = { method, body, headers };
+const maxPageSize = 50;
+const wait: (period: number) => Promise<void> =
+  async (period) => new Promise((resolve) => {setTimeout(resolve, period)})
+const retry: (fn: AsyncFn, times: number, delay: number) => Promise<any> =
+  async (fn, times, delay) => {
     try {
-      const response = await retry(
-        async () => await timedFetch(url, options),
-        retries,
-        delay
-      );
-      if (response.ok || ignore_errors.some((x) => x == response.status)) {
-        info(`Got response ${response.status} for ${method} ${url}`);
-        return response;
+      return await fn();
+    } catch (e: any) {
+      if (times <= 0) {
+        debug(e.message);
+        throw e;
       } else {
-        const message = `Unexpected response ${response.status} for ${method} ${url}`;
-        debug(await response.text());
+        await wait(delay);
+        await retry(fn, times - 1, delay);
+      }
+    }
+  }
+const timedFetch: (resource: string, options: any) => Promise<any> =
+  async (resource, options) => {
+    const { timeout = TIMEOUT_DEFAULT } = options;
+    const controller = new AbortController();
+    const signal = controller.signal;
+    const id = setTimeout(() => controller.abort(), timeout);
+    const response = await fetch(resource, { ...options, signal });
+    clearTimeout(id);
+    return response;
+  }
+const api: (options: ApiOptions) => Promise<any> =
+  async (options) => {
+    const {
+      token,
+      path,
+      method = 'GET',
+      body = null,
+      ignore_errors = [],
+      retries = RETRIES_DEFAULT,
+      delay = DELAY_DEFAULT
+    } = options;
+    const uri = `https://api.cloudflare.com/client/v4${path}`;
+    info(`Fetching ${method} ${uri}`);
+    async function fetchData(url: string) {
+      const headers = {
+        'Content-Type': 'application/json',
+        Authorization: `Bearer ${token}`
+      };
+      const options = { method, body, headers };
+      try {
+        const response = await retry(
+          async () => await timedFetch(url, options),
+          retries,
+          delay
+        );
+        if (response.ok || ignore_errors.some((x) => x == response.status)) {
+          info(`Got response ${response.status} for ${method} ${url}`);
+          return response;
+        } else {
+          const message = `Unexpected response ${response.status} for ${method} ${url}`;
+          debug(await response.text());
+          error(message);
+          throw new Error(message);
+        }
+      } catch (e: any) {
+        const message = `Unrecoverable error for ${method} ${url}: ${e.message}`;
         error(message);
         throw new Error(message);
       }
-    } catch (e: any) {
-      const message = `Unrecoverable error for ${method} ${url}: ${e.message}`;
-      error(message);
-      throw new Error(message);
     }
+    const params = new URLSearchParams(path.split('?')[1]);
+    const response = await fetchData(uri);
+    const data: any = response ? await response.json() : { result: [] };
+    const pages = data.result_info?.total_pages;
+    if (pages > 1 && !params.has('page')) {
+      const range = [...Array(pages - 1).keys()].map((x) => x + 2);
+      const pageRequests = range.map(async (page) => {
+        const separator = uri.includes('?') ? '&' : '?';
+        const pageUri = `${uri}${separator}page=${page}`;
+        const pageResponse = await fetchData(pageUri);
+        return pageResponse ? pageResponse.json() : { result: [] };
+      });
+      const pageData = await Promise.all(pageRequests);
+      const result = data.result.concat(pageData.flatMap((x) => x.result));
+      info(`Got ${result.length} results for ${method} ${uri}`);
+      return {
+        result_info: {
+          total_pages: pages,
+          per_page: data.result_info.per_page
+        },
+        success: true,
+        errors: [],
+        messages: [],
+        result
+      };
+    }
+    return data;
   }
-  const params = new URLSearchParams(path.split('?')[1]);
-  const response = await fetchData(uri);
-  const data: any = response ? await response.json() : { result: [] };
-  const pages = data.result_info?.total_pages;
-  if (pages > 1 && !params.has('page')) {
-    const range = [...Array(pages - 1).keys()].map((x) => x + 2);
-    const pageRequests = range.map(async (page) => {
-      const separator = uri.includes('?') ? '&' : '?';
-      const pageUri = `${uri}${separator}page=${page}`;
-      const pageResponse = await fetchData(pageUri);
-      return pageResponse ? pageResponse.json() : { result: [] };
-    });
-    const pageData = await Promise.all(pageRequests);
-    const result = data.result.concat(pageData.flatMap((x) => x.result));
-    info(`Got ${result.length} results for ${method} ${uri}`);
-    return {
-      result_info: {
-        total_pages: pages,
-        per_page: data.result_info.per_page
-      },
-      success: true,
-      errors: [],
-      messages: [],
-      result
-    };
-  }
-  return data;
-}
 
-export { api };
+export { api, maxPageSize };
