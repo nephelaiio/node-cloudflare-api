0346b4e19b0a463615cbd907ef287387a258488e
Ted Cook <teodoro.cook@gmail.com>
2023-09-15 11:55:40 -0600

    Fix build errors

--- src/zones.ts
+++ src/zones.ts
@@ -3,19 +3,19 @@
 import type { ApiFunction } from './api';
 import { api, maxPageSize } from './api';
 
-type zoneOptions = {
+type ZoneOptions = {
   token: string;
   zone?: string;
   exec?: ApiFunction;
 };
 
-const zoneInfo: (options: ZoneOptions) => Promise<[any]> = (
-  options: zoneOptions
+const zoneInfo: (options: ZoneOptions) => Promise<any[]> = async (
+  options: ZoneOptions
 ) => {
   const { token, zone = null, exec = api } = options;
   const path = zone ? `/zones?name=${zone}` : `/zones?per_page=${maxPageSize}`;
   const zones = await exec({ token, path });
-  return [zones.result].flat();
+  return Promise.resolve([zones.result].flat());
 };
 
 export { zoneInfo };
