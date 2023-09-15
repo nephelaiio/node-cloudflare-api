f4adc94ed4887c65b129d4017f0e479c378279b8
Ted Cook <teodoro.cook@gmail.com>
2023-09-15 11:36:54 -0600

    Fix lint errors

--- src/zone.mock.test.ts
+++ src/zone.mock.test.ts
@@ -6,16 +6,16 @@ setVerbose();
 
 const token = 'token';
 const zones = [
-    { id: '00000000000000000000000000000001', name: 'zone01' },
-    { id: '00000000000000000000000000000002', name: 'zone02' }
-]
-const execList = mock((_) => Promise.resolve({ result: zones }))
-const execInfo = mock ((_) => Promise.resolve({ result: zones[0]}))
+  { id: '00000000000000000000000000000001', name: 'zone01' },
+  { id: '00000000000000000000000000000002', name: 'zone02' }
+];
+const execList = mock((_) => Promise.resolve({ result: zones }));
+const execInfo = mock((_) => Promise.resolve({ result: zones[0] }));
 
 test('full zone list should return all zones', async () => {
-    expect(await zoneInfo({ token, exec: execList})).toEqual(zones);
+  expect(await zoneInfo({ token, exec: execList })).toEqual(zones);
 });
 
 test('single zone list should return single zone info', async () => {
-    expect(await zoneInfo({ token, exec: execInfo})).toEqual([zones[0]]);
+  expect(await zoneInfo({ token, exec: execInfo })).toEqual([zones[0]]);
 });
--- src/zones.ts
+++ src/zones.ts
@@ -1,19 +1,21 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
 
-import type { ApiFunction } from './api'
-import { api, maxPageSize } from './api'
+import type { ApiFunction } from './api';
+import { api, maxPageSize } from './api';
 
 type zoneOptions = {
-    token: string;
-    zone?: string;
-    exec?: ApiFunction;
-}
+  token: string;
+  zone?: string;
+  exec?: ApiFunction;
+};
 
-const zoneInfo: (options: ZoneOptions) => Promise<[any]> = (options: zoneOptions) => {
-    const  { token, zone = null, exec = api } = options;
-    const path = zone ? `/zones?name=${zone}` : `/zones?per_page=${maxPageSize}`;
-    const zones = await exec({ token, path });
-    return [zones.result].flat();
-}
+const zoneInfo: (options: ZoneOptions) => Promise<[any]> = (
+  options: zoneOptions
+) => {
+  const { token, zone = null, exec = api } = options;
+  const path = zone ? `/zones?name=${zone}` : `/zones?per_page=${maxPageSize}`;
+  const zones = await exec({ token, path });
+  return [zones.result].flat();
+};
 
-export { zoneInfo }
+export { zoneInfo };
