714b99415c8f35d594ac22e5fbbd0457c7a73020
Ted Cook <teodoro.cook@gmail.com>
2023-09-16 22:29:39 -0600

    Fix module import location

--- src/all.ts
+++ src/all.ts
@@ -1,5 +1,5 @@
 import { maxPageSize, api } from './api';
 import { zoneInfo } from './zones';
-import { wafPackageList } from './waf/packages';
+import { wafPackageList } from './waf/package';
 
 export { maxPageSize, api, zoneInfo, wafPackageList };
