afbc7ebf52c3b0aa6a6cd3d52d38812065b44385
Teodoro Cook <teodoro.cook@gmail.com>
2023-09-17 18:13:26 -0600

    Add wafPackageList method (#17)

---
+++ src/waf/package.ts
@@ -0,0 +1,66 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
+import type { ApiFunction } from '../api';
+import { api, maxPageSize } from '../api';
+import { zoneInfo } from '../zone';
+import { info } from '@nephelaiio/logger';
+
+type PackageOptions = {
+  token: string;
+  zone?: string;
+  packageName?: string;
+  exec?: ApiFunction;
+};
+
+const wafPackageList: (options: PackageOptions) => Promise<any[]> = async (
+  options
+) => {
+  const zoneMessage = options.zone ? `zone ${options.zone}` : 'all zones';
+  const packageMessage = options.packageName
+    ? `waf packages matching regex ${options.packageName}`
+    : 'all waf packages';
+  const message = `Fetching ${packageMessage} for ${zoneMessage}`;
+  info(message);
+  const { token, zone = undefined, packageName = null, exec = api } = options;
+  const zones = await zoneInfo({ token, zone, exec });
+  const packageQuery = zones.map(async (z) => {
+    const zoneId = (await z).id;
+    const zoneName = (await z).name;
+    const addZoneName = (p: any) => ({ ...p, ...{ zone_name: zoneName } });
+    const path = `/zones/${zoneId}/firewall/waf/packages?per_page=${maxPageSize}`;
+    const result = (await exec({ token, path, ignore_errors: [403] })).result;
+    const packages = packageName
+      ? (result || []).filter((p: any) => p.name == packageName)
+      : result;
+    return packages.map(addZoneName);
+  });
+  const results = await Promise.all(packageQuery);
+  const packages = results.flat();
+  info(`Retrieved ${packages.length} packages`);
+  return packages;
+};
+
+// const wafPackageRules: (options: PackageOptions) => Promise<any> = async (
+//   options
+// ) => {
+//   const zoneMessage = options.zone ? `zone ${options.zone}` : 'all zones';
+//   const packageMessage = options.packageName
+//     ? ` matching package name ${packageName(options)}`
+//     : '';
+//   info(`Fetching waf rules for ${zoneMessage}${packageMessage}`);
+//   const packages = await wafPackageList(options);
+//   const token = apiToken();
+//   const ruleQuery = packages.map(async (p) => {
+//     const packageId = (await p).id;
+//     const zoneId = (await p).zone_id;
+//     const zoneName = (await p).zone_name;
+//     const addZoneName = (p: any) => ({ ...p, ...{ zone_name: zoneName } });
+//     const path = `/zones/${zoneId}/firewall/waf/packages/${packageId}/rules?per_page=50`;
+//     const result = (await api({ token, path })).result;
+//     info(`Retrieved ${result.length} rules`);
+//     return result.map(addZoneName);
+//   });
+//   const rules = await Promise.all(ruleQuery);
+//   return rules.flat();
+// };
+
+export { wafPackageList };
--- src/zone.mock.test.ts
+++ src/zone.mock.test.ts
@@ -1,5 +1,6 @@
-import { zoneInfo } from './zones';
-import { setVerbose } from '@nephelaiio/logger';
+import { zoneInfo } from './zone';
+import { maxPageSize } from './api';
+import { info, error, setVerbose } from '@nephelaiio/logger';
 import { expect, test, mock } from 'bun:test';
 
 setVerbose();
@@ -9,13 +10,30 @@ const zones = [
   { id: '00000000000000000000000000000001', name: 'zone01' },
   { id: '00000000000000000000000000000002', name: 'zone02' }
 ];
-const execList = mock((_) => Promise.resolve({ result: zones }));
-const execInfo = mock((_) => Promise.resolve({ result: zones[0] }));
+const exec = mock(({ path }) => {
+  info(`Mocking api call to path ${path}`);
+  switch (path) {
+    case `/zones?per_page=${maxPageSize}`:
+      return Promise.resolve({ result: zones });
+    case `/zones?name=${zones[0].name}`:
+      return Promise.resolve({ result: zones[0] });
+    case `/zones?name=${zones[1].name}`:
+      return Promise.resolve({ result: zones[1] });
+    default:
+      error(`Unexpected mocked api call to path ${path}`);
+      process.exit(1);
+  }
+});
 
 test('full zone list should return all zones', async () => {
-  expect(await zoneInfo({ token, exec: execList })).toEqual(zones);
+  expect(await zoneInfo({ token, exec })).toEqual(zones);
 });
 
 test('single zone list should return single zone info', async () => {
-  expect(await zoneInfo({ token, exec: execInfo })).toEqual([zones[0]]);
+  expect(await zoneInfo({ token, exec, zone: zones[0].name })).toEqual([
+    zones[0]
+  ]);
+  expect(await zoneInfo({ token, exec, zone: zones[1].name })).toEqual([
+    zones[1]
+  ]);
 });
